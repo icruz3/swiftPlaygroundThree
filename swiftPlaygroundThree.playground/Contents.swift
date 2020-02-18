e3af35d9ecdf227d204c27e778195cafd07d3952
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-02-18 10:20:45 -1000

    part seven is completed

--- swiftPlaygroundThree.playground/Contents.swift
+++ swiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
+//swift playground three intro to arrryas 2.18.2020
+//part 2
 
-var str = "Hello, playground"
+var fruitNames: [String]
+
+//part 3
+
+fruitNames = ["kiwi", "apples", "watermelon"]
+
+
+// part 4
+
+print (fruitNames)
+
+// part 5
+// experiment with interpolation
+
+print("I like to eat\(fruitNames)")
+
+for name in fruitNames
+    
+{
+print("I like to eat" + name)
+    
+}
+
+//part 6
+
+for kiwi in fruitNames
+{
+   print("I like to eat " + kiwi)
+}
+
+//part 7 array indexes
+
+print(fruitNames [1])
+
+//problem sets
+
+var carNames: [String]
+
+
